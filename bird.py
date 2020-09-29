b08691da90bd1c71b03baa1329dd355e42438804
victorakaps <49690593+victorakaps@users.noreply.github.com>
2020-09-30 01:32:28 +0530

    adjusted weight

--- bird.py
+++ bird.py
@@ -1,6 +1,8 @@
 import pygame
 import random
+import numpy as np
 from defs import *
+from nnet import Nnet
 
 
 class Bird():
@@ -11,6 +13,15 @@ class Bird():
         self.img = pygame.image.load(BIRD_FILENAME)
         self.rect = self.img.get_rect()
         self.speed = 0
+        self.fitness = 0
+        self.time_lived = 0
+        self.nnet = Nnet(NNET_INPUTS, NNET_HIDDEN, NNET_OUTPUTS)
+        self.set_position(BIRD_START_X, BIRD_START_Y)
+
+    def reset(self):
+        self.state = BIRD_ALIVE
+        self.speed = 0
+        self.fitness = 0
         self.time_lived = 0
         self.set_position(BIRD_START_X, BIRD_START_Y)
 
@@ -33,8 +44,11 @@ class Bird():
             self.rect.top = 0
             self.speed = 0
 
-    def jump(self):
-        self.speed = BIRD_START_SPEED
+    def jump(self, pipes):
+        inputs = self.get_inpputs(pipes)
+        val = self.nnet.get_max_value(inputs)
+        if val > JUMP_CHANCE:
+            self.speed = BIRD_START_SPEED
 
     def draw(self):
         self.gameDisplay.blit(self.img, self.rect)
@@ -42,19 +56,112 @@ class Bird():
     def check_status(self, pipes):
         if self.rect.bottom > DISPLAY_H:
             self.state = BIRD_DEAD
-            print('Birdie died')
         else:
             self.check_hits(pipes)
+
+    def assign_collision_fitness(self, p):
+        gap_y = 0
+        if p.pipe_type == PIPE_UPPER:
+            gap_y = p.rect.bottom + PIPE_GAP_SIZE / 2
+        else:
+            gap_y = p.rect.top - PIPE_GAP_SIZE / 2
+
+        self.fitness = -(abs(self.rect.centery - gap_y))
 
     def check_hits(self, pipes):
         for p in pipes:
             if p.rect.colliderect(self.rect):
                 self.state = BIRD_DEAD
+                self.assign_collision_fitness(p)
                 break
 
     def update(self, dt, pipes):
         if self.state == BIRD_ALIVE:
             self.time_lived += dt
             self.move(dt)
+            self.jump(pipes)
             self.draw()
             self.check_status(pipes)
+
+    def get_inpputs(self, pipes):
+        closet = DISPLAY_W * 2
+        bottom_y = 0
+        for p in pipes:
+            if p.pipe_type == PIPE_UPPER and p.rect.right < closet and p.rect.right > self.rect.left:
+                closet = p.rect.right
+                bottom_y = p.rect.bottom
+
+        horizontal_distance = closet - self.rect.centerx
+        vertical_distance = (self.rect.centery) - \
+            (bottom_y + PIPE_GAP_SIZE / 2)
+        inputs = [
+            ((horizontal_distance / DISPLAY_W) * 0.99) + 0.01,
+            (((vertical_distance + Y_SHIFT) / NORMALIZER) * 0.99)
+        ]
+        return inputs
+
+    def create_offspring(p1, p2, gameDisplay):
+        new_bird = Bird(gameDisplay)
+        new_bird.nnet.create_mixed_weights(p1.nnet, p2.nnet)
+        return new_bird
+
+
+class BirdCollection():
+
+    def __init__(self, gameDisplay):
+        self.gameDisplay = gameDisplay
+        self.birds = []
+        self.create_new_generation()
+
+    def create_new_generation(self):
+        self.birds = []
+        for i in range(0, GENERATION_SIZE):
+            self.birds.append(Bird(self.gameDisplay))
+
+    def update(self, dt, pipes):
+        num_alive = 0
+        for b in self.birds:
+            b.update(dt, pipes)
+            if b.state == BIRD_ALIVE:
+                num_alive += 1
+
+        return num_alive
+
+    def evolve_population(self):
+        for b in self.birds:
+            b.fitness += b.time_lived * PIPE_SPEED
+
+        self.birds.sort(key=lambda x: x.fitness, reverse=True)
+
+        # for b in self.birds[0:10]:
+        #     print("FITNESS: ", b.fitness)
+
+        cut_off = int(len(self.birds) * MUTATION_CUT_OFF)
+        good_birds = self.birds[0:cut_off]
+        bad_birds = self.birds[cut_off:]
+        num_bad_to_take = int(len(self.birds) * MUTATION_BAD_TO_KEEP)
+
+        for b in bad_birds:
+            b.nnet.modify_weights()
+
+        new_birds = []
+
+        idx_bad_to_take = np.random.choice(np.arrange(
+            len(bad_birds)), num_bad_to_take, replace=False)
+
+        for index in idx_bad_to_take:
+            new_birds.append(bad_birds[index])
+
+        new_birds.extend(good_birds)
+        children_neede = len(self.birds) - len(new_birds)
+
+        while len(new_birds) < len(self.birds):
+            idx_to_breed = np.random.choice(
+                np.arange(len(good_birds)), 2, replace=False)
+            if idx_to_breed[0] != idx_to_breed[1]:
+                new_bird = Bird.create_offspring(
+                    good_birds[idx_to_breed[0]], good_birds[idx_to_breed[1]], self.gameDisplay)
+                if random.random() < MUTATION_MODIFY_CHANCE_LIMIT:
+                    new_bird.nnet.modify_weights()
+
+        self.create_new_generation()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from defs import *
 from pipe import PipeCollection
-from bird import Bird
+from bird import BirdCollection
 
 
 def update_label(data, title, font, x, y, gameDisplay):
@@ -10,7 +10,7 @@ def update_label(data, title, font, x, y, gameDisplay):
     return y
 
 
-def update_data_labels(gameDisplay, dt, game_time, num_iterations, font):
+def update_data_labels(gameDisplay, dt, game_time, num_iterations, num_alive, font):
     y_pos = 10
     gap = 20
     x_pos = 10
@@ -19,6 +19,8 @@ def update_data_labels(gameDisplay, dt, game_time, num_iterations, font):
     y_pos = update_label(round(game_time/1000, 2), "Game time",
                          font, x_pos, y_pos + gap, gameDisplay)
     y_pos = update_label(num_iterations, "Iteration",
+                         font, x_pos, y_pos + gap, gameDisplay)
+    y_pos = update_label(num_alive, "Alive",
                          font, x_pos, y_pos + gap, gameDisplay)
 
 
@@ -33,7 +35,7 @@ def run_game():
     pipes = PipeCollection(gameDisplay)
     pipes.create_new_set()
 
-    bird = Bird(gameDisplay)
+    birds = BirdCollection(gameDisplay)
 
     label_font = pygame.font.SysFont("monospace", DATA_FONT_SIZE)
 
@@ -53,21 +55,19 @@ def run_game():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    bird.jump()
-                else:
                     running = False
 
         pipes.update(dt)
-        bird.update(dt, pipes.pipes)
+        num_alive = birds.update(dt, pipes.pipes)
 
-        if bird.state == BIRD_DEAD:
+        if num_alive == 0:
             pipes.create_new_set()
             game_time = 0
-            bird = Bird(gameDisplay)
+            birds.evolve_population()
             num_iterations += 1
 
-        update_data_labels(gameDisplay, dt, game_time, num_iterations, label_font)
+        update_data_labels(gameDisplay, dt, game_time,
+                           num_iterations, num_alive, label_font)
         pygame.display.update()
 
 
